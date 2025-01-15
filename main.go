9300fa7b4df02a5590d7920062c78788ed18d52a
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-01-16 01:06:36 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"learning/utils"
+)
+
+func main()  {
+	fmt.Println("Hello GO")
+	utils.UtilFunction("Hello world form utils")
+}
---
+++ utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+import (
+	"fmt"
+)
+
+func UtilFunction(message string) {
+    fmt.Println(message)
+}
